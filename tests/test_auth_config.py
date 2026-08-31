08bf92602de6ce351bfb07e7b1dceaf068fe0afc
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-08-31 16:18:10 +0300

    Add config.py:

---
+++ tests/test_auth_config.py
@@ -0,0 +1,15 @@
+import requests
+
+from config import API_TOKEN, BASE_URL
+
+def test_get_users():
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+
+    response = requests.get(
+        f"{BASE_URL}/users",
+        headers=headers
+    )
+
+    assert response.status_code == 200
