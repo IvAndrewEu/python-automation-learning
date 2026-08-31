59c4db4c63c63becf6ee1e917f4e0fd77b0a7e34
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-08-31 16:44:15 +0300

    ___

--- tests/test_auth_config.py
+++ tests/test_auth_config.py
@@ -8,8 +8,10 @@ def test_get_users():
     }
 
     response = requests.get(
-        f"{BASE_URL}/users",
+        f"{BASE_URL}/headers",
         headers=headers
     )
 
     assert response.status_code == 200
+    data = response.json()
+    print(data)
