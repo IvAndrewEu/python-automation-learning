351c97d3e8c832a14162db2c592fda4e32100842
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-01 16:11:10 +0300

    Add /bearer

--- tests/test_auth_config.py
+++ tests/test_auth_config.py
@@ -8,16 +8,17 @@ def test_get_headers():
     }
 
     response = requests.get(
-        f"{BASE_URL}/headers",
+        f"{BASE_URL}/bearer",
         headers=headers
     )
 
     assert response.status_code == 200
     data = response.json()
-    print(data)
+    print(response.json())
+    assert data["headers"]["Authorization"] == f"Bearer {API_TOKEN}"
 
 def test_get_headers_without_token():
-    response = requests.get(f"{BASE_URL}/headers")
+    response = requests.get(f"{BASE_URL}/bearer")
 
     print(response.status_code)
     print(response.json())
