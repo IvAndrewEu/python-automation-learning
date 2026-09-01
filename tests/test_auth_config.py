5137078f76d3bc201fe48fada07d0d936833ded4
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-01 17:09:26 +0300

    Add new ids in test_auth_config.py

--- tests/test_auth_config.py
+++ tests/test_auth_config.py
@@ -8,8 +8,13 @@ from config import API_TOKEN, BASE_URL
 @pytest.mark.parametrize(
     "headers, expected_status",
     [({"Authorization": f"Bearer {API_TOKEN}"}, 200),
-    ({"Authorization": f"Bearer wrong token"}, 200),
-    ({}, 401)]
+    ({"Authorization": f"Bearer wrong token"}, 200), #тут тоже должен быть статус код 401, но из-за условностей тестового контура тут статус код 200
+    ({}, 401)],
+    ids=[
+        "test_1",
+        "test_2",
+        "test_3"
+    ]
 )
 def test_get_headers(headers, expected_status):
     response = requests.get(
