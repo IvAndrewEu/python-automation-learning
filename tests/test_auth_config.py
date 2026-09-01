a8e87a6f08097474bac899f5a10a106f09b81ab3
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-01 17:04:45 +0300

    fix status_code

--- tests/test_auth_config.py
+++ tests/test_auth_config.py
@@ -8,8 +8,8 @@ from config import API_TOKEN, BASE_URL
 @pytest.mark.parametrize(
     "headers, expected_status",
     [({"Authorization": f"Bearer {API_TOKEN}"}, 200),
-    ({"Authorization": f"Bearer wrong token"}, 401),
-     ({}, 401)]
+    ({"Authorization": f"Bearer wrong token"}, 200),
+    ({}, 401)]
 )
 def test_get_headers(headers, expected_status):
     response = requests.get(
