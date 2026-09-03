64143807274f0e83948bdcf45ceeb52f1cd7d531
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-03 12:26:48 +0300

    add parametrize in tests/test_body_validation.py

--- tests/test_body_validation.py
+++ tests/test_body_validation.py
@@ -1,19 +1,29 @@
 import requests
+import pytest
 
 from config import BASE_URL
 
-def test_post_body():
-    payload = {
-        "name": "Andrew",
-        "age": 27
-    }
+@pytest.mark.parametrize(
+    "payload",
+    [
+        ({"name": "Andrew", "age": 27}),
+        ({"age": 27}),
+        ({"name": "Andrew", "age": "twenty seven"})
+    ],
+    ids=[
+        "valid_test",
+        "without_name",
+        "wrong_age_type"
+    ]
+)
+def test_post_body(payload):
+
 
     response = requests.post(
         f"{BASE_URL}/anything",
         json=payload
     )
 
-    assert response.status_code == 200
     data = response.json()
-    assert data["json"]["age"] == 27, f"data['json']['age'] != 27)"
-    assert data["json"]["name"] == "Andrew", f"data['json']['name'] != Andrew"
+    assert response.status_code == 200
+    assert data["json"] == payload
