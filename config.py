e4ccded71cc0a1e9220e482da94f8068c5fab410
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-03 13:19:59 +0300

    add new paramet in config.py

--- config.py
+++ config.py
@@ -2,9 +2,13 @@ import os
 
 BASE_URL = os.getenv("BASE_URL")
 API_TOKEN = os.getenv("API_TOKEN")
+REQRES_API_KEY = os.getenv("REQRES_API_KEY")
 
 if BASE_URL is None:
     raise ValueError("BASE_URL is not set")
 
 if API_TOKEN is None:
     raise ValueError("API_TOKEN is not set")
+
+if REQRES_API_KEY is None:
+    raise ValueError("REQRES_API_KEY is not set")
