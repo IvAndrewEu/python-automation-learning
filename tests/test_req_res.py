94093f27f094c74ed281d1ad61538e34fee3bfc4
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-06 17:16:47 +0300

    add new parametrize case

--- tests/test_req_res.py
+++ tests/test_req_res.py
@@ -71,6 +71,7 @@ from config import REQRES_API_KEY
         "empty_body",
         "wrong_email_type",
         "wrong_password_type",
+        "wrong_username_type"
     ]
 )
 def test_post_body(payload, expected_status, message_error):
