72a0409cb51aad45e089ab0a326e633c4df6d8c7
Andrew Ivanov <ivanovandrew031198@gmail.com>
2026-09-13 21:22:05 +0300

    in test_session_headers add new asserts

--- tests/test_session_headers.py
+++ tests/test_session_headers.py
@@ -2,6 +2,8 @@
 
 def test_session_headers(session):
     response = session.get("https://httpbin.org/headers")
-
     data = response.json()
     print(data)
+
+    assert data["X-Test-Client"]
+    assert data["X-Test-Client"] == "python-aqa"
